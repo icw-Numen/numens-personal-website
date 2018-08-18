bb60b52a2dcacc8a1573fc368ba22fa4a669955d
icw-Numen <igchenwu@gmail.com>
2018-08-18 05:05:32 +0000

    css updates

---
+++ v1/app.js
@@ -0,0 +1,50 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    flash = require('connect-flash'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    //mongoose = require('mongoose'),
+    middleware = require('./middleware');
+
+// requiring routes
+var indexRoutes = require("./routes/index");
+    
+// connect to db
+//var dbUrl = process.env.DATABASEURL;
+//mongoose.connect(dbUrl, { useNewUrlParser: true });
+
+// changing some Express settings
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+app.use(flash());
+
+// Passport settings
+app.use(require("express-session")({
+    secret: "the sun will always rise again",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//passport.use(new LocalStrategy(User.authenticate()));
+//passport.serializeUser(User.serializeUser());
+//passport.deserializeUser(User.deserializeUser());
+
+// setting some variables to be shared across all routes
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.success = req.flash('success');
+   res.locals.error = req.flash('error');
+   next();
+});
+
+// setting up routes
+app.use('/', indexRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function() {
+   console.log('numen\'s personal website\'s server online');
+});
---
+++ v1/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+//root route
+router.get('/', function(req, res) {
+    res.render('home');
+});
+
+router.get('/projects', function(req, res) {
+    res.render('projects');
+});
+
+router.get('/about', function(req, res) {
+    res.render('about');
+});
+
+router.get('/contact', function(req, res) {
+    res.render('contact');
+});
+
+router.get('/hire-me', function(req, res) {
+    res.render('hireme');
+});
+
+router.get('/donate', function(req, res) {
+    res.render('donate');
+});
+
+//root route
+router.get('*', function(req, res) {
+    res.render('home');
+});
+
+module.exports = router;
