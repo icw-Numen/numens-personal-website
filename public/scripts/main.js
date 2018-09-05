1bf721efd0132b1012b5b90cd1f4f647577e23ae
icw-Numen <igchenwu@gmail.com>
2018-09-05 22:29:49 +0000

    minor changes to show bg button

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,17 +2,17 @@
 $('.center').stop(true).fadeIn({ duration: 1000, queue: false }).css('display', 'none').slideDown(700);
 
 // fade out page content when leaving page
-$("a").click(function(event){
+$('a').click(function(event){
     event.preventDefault();
     linkLocation = this.href;
-    $(".center").fadeOut({ duration: 1000, queue: false }).slideUp(700, redirectPage(linkLocation));      
+    $('.center').fadeOut({ duration: 1000, queue: false }).slideUp(700, redirectPage(linkLocation));
 });
 
 // add events to button to hide/show page contents
-$("button.toggleBg").click(function(event) {
-    $("button.toggleBg i").toggleClass("fas fa-eye-slash");
-    $("button.toggleBg i").toggleClass("fas fa-eye");
-    $(".center").fadeToggle(700);
+$('button.toggleBg').click(function(event) {
+    $('button.toggleBg i').toggleClass('fas fa-eye-slash');
+    $('button.toggleBg i').toggleClass('fas fa-eye');
+    $('.center').fadeToggle(700);
 });
 
 function redirectPage(link) {
