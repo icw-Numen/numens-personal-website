530ffed64d39a65c58698541e3ee6f7805b6cce9
icw-Numen <igchenwu@gmail.com>
2018-09-19 22:30:29 +0000

    removed some animations

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,13 +5,13 @@ $(`.navbar a[href='${window.location.pathname}']`).css('color', 'var(--selected-
 $('.center').stop(true).fadeIn({ duration: 1000, queue: false }).css('display', 'none').slideDown(700);
 
 // fade out page content when leaving page
-$('a').click(function(event){
-    event.preventDefault();
-    linkLocation = this.href;
-    $('.center').fadeOut({ duration: 1000, queue: false }).slideUp(700, function() {
-        setTimeout(function(){ redirectPage(linkLocation); }, 200);
-    });
-});
+// $('a').click(function(event){
+//     event.preventDefault();
+//     linkLocation = this.href;
+//     $('.center').fadeOut({ duration: 1000, queue: false }).slideUp(700, function() {
+//         setTimeout(function(){ redirectPage(linkLocation); }, 200);
+//     });
+// });
 
 // add events to button to hide/show page contents
 $('.toggleBg').on('click', function() {
