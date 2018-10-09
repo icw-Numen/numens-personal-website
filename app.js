4ca83714385ea8074cd1f34f6ace09f29470224a
icw-Numen <igchenwu@gmail.com>
2018-10-09 23:03:16 +0000

    https

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use('/', indexRoutes);
 
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
     return res.redirect('https://' + req.get('host') + req.url);
   }
   next();
