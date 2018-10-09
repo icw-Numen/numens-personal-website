754679bed19c768ae08a081b0baaf05b2fb51f65
icw-Numen <igchenwu@gmail.com>
2018-10-09 21:10:34 +0000

    updated static files

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var port = process.env.PORT,
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(flash());
 
